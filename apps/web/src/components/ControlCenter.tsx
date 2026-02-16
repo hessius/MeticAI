f504d1e247c02419db1e676200f54abfb428c717
hessius <hessius@me.com>
2026-02-16 22:59:46 +0100

    fix: Swedish waiting-for-shot translation and warmup cancel toast

--- apps/web/src/components/ControlCenter.tsx
+++ apps/web/src/components/ControlCenter.tsx
@@ -322,7 +322,7 @@ export function ControlCenter({ machineState, onOpenLiveView }: ControlCenterPro
                 size="sm"
                 className="flex-1 min-w-0 h-9 text-xs"
                 disabled={!machineState.connected}
-                onClick={() => cmd(abortShot, 'preheatCancelled')}
+                onClick={() => cmd(abortShot, isPreheating ? 'preheatCancelled' : 'warmupCancelled')}
               >
                 <XCircle size={14} weight="fill" className="mr-1 shrink-0" />
                 {t('controlCenter.actions.abortPreheat')}
